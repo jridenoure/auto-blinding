f2404eb277d19a5993f950208b41634c4f38cc46
jridenoure <147731760+jridenoure@users.noreply.github.com>
2025-04-21 10:34:03 -0500

    Add files via upload

---
+++ timestamps.py
@@ -0,0 +1,37 @@
+import pandas
+import pydicom
+import os
+import numpy
+from tkinter import filedialog
+workingDir = filedialog.askdirectory()+"/"
+files = os.listdir(workingDir)
+df=pandas.DataFrame()
+base= os.path.basename(os.path.dirname(workingDir))
+for file in files:
+    dsFull = pydicom.dcmread(workingDir + file)
+    time=(pydicom.dataelem.convert_raw_data_element(dsFull.get_item([0x0008,0x0033]))).value
+    #print(type(dsFull))
+    ds1 = (dsFull.get_item([0x7fe1, 0x1001]))
+    ds1 = pydicom.dataelem.convert_raw_data_element(ds1)
+    #print("ds1 \n", type(ds1))
+    ds2 = ds1[0].get_item([0x7fe1, 0x1073])
+    ds2 = pydicom.dataelem.convert_raw_data_element(ds2)
+    #print("ds2 \n", type(ds2))
+    ds3 = ds2[0].get_item([0x7fe1, 0x1075])
+    ds3 = pydicom.dataelem.convert_raw_data_element(ds3)
+    #print("ds3 \n", type(ds3))
+    ds4 = ds3[-1].get_item([0x7fe1, 0x1057]).value
+
+    #print("ds2 \n", type(ds2))
+    #print("ds3 \n", type(ds3))
+    #print("ds4 \n", type(ds4))
+    #print(ds4)
+    ID = dsFull.PatientID
+    imgData = list((file,ID,ds4,time))
+    imgData2 = list((ID,ds4))
+    dataSeries = (pandas.Series(data=imgData)).T
+    df = pandas.concat([df,dataSeries.to_frame().T],ignore_index=True)
+    #print(df)
+    #print("iterator: \n", file)
+
+print(df.sort_values(by=3))
